ead68398ae96199c92cbd359dc0556b67a9ba7f0
Yannis Irkakene <yannis@irkakene.com>
2024-02-17 17:45:46 +0100

    added LoginScreen class and SignupScreen class

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import Client.Components.LoginScreen;
+import Client.Components.SignupScreen;
+
+public class Main {
+    public static void main(String[] args) {
+        LoginScreen loginScreen = new LoginScreen();
+//        SignupScreen signupScreen = new SignupScreen();
+    }
+}
