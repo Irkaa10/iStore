327833d9a01fd3335ea02ad85a75cfd7220ebaeb
Irkaa10 <149479479+Irkaa10@users.noreply.github.com>
2024-02-20 15:52:20 +0100

    Update InventoryPanel.java

--- src/Client/Components/InventoryPanel.java
+++ src/Client/Components/InventoryPanel.java
@@ -1,5 +1,8 @@
 package Client.Components;
 
+import Client.Components.Frames.IncreaseStockFrame;
+import Client.Components.Frames.ReduceStockFrame;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
@@ -12,20 +15,81 @@ public class InventoryPanel extends JPanel {
 
     public InventoryPanel() {
         setLayout(new BorderLayout());
+        setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
-        JLabel label = new JLabel("Inventory Panel");
-        label.setHorizontalAlignment(JLabel.CENTER);
-        add(label, BorderLayout.CENTER);
+        Object[][] initialInventoryData = {
+                {"Item A", "10", "19.99"},
+                {"Item B", "45", "9.99"},
+        };
+        Object[] inventoryColumnNames = {"Name","Stock", "Price"};
+
+        currentTableModel = new DefaultTableModel(initialInventoryData, inventoryColumnNames);
+
+        JTable table = new JTable(currentTableModel);
+        JScrollPane scrollPane = new JScrollPane(table);
+        add(scrollPane, BorderLayout.CENTER);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
+
+        JButton increaseStockButton = new JButton("Increase stock");
+        increaseStockButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int selectedRow = table.getSelectedRow();
+
+                if (selectedRow != -1) {
+                    new IncreaseStockFrame(currentTableModel, table, selectedRow);
+                } else {
+                    JOptionPane.showMessageDialog(InventoryPanel.this, "Please select an item to increase stock.");
+                }
+            }
+        });
+        buttonPanel.add(increaseStockButton);
+
+        JButton reduceStockButton = new JButton("Reduce stock");
+        reduceStockButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int selectedRow = table.getSelectedRow();
+
+                if (selectedRow != -1) {
+                    new ReduceStockFrame(currentTableModel, table, selectedRow);
+                } else {
+                    JOptionPane.showMessageDialog(InventoryPanel.this, "Please select an item to reduce stock.");
+                }
+            }
+        });
+        buttonPanel.add(reduceStockButton);
 
         JButton addProductButton = new JButton("Add Product");
         addProductButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Implement logic to add product
-                JOptionPane.showMessageDialog(null, "Product added!");
+                // TODO: CODE SQL ADD USER
             }
         });
-        add(addProductButton, BorderLayout.NORTH);
+        buttonPanel.add(addProductButton);
+
+        JButton editProductButton = new JButton("Edit Product");
+        editProductButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO: CODE FOR EDITING USER
+            }
+        });
+        buttonPanel.add(editProductButton);
+
+        JButton deleteProductButton = new JButton("Delete Product");
+        deleteProductButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO: CODE FOR DELETING USER
+            }
+        });
+        buttonPanel.add(deleteProductButton);
+
+        add(buttonPanel, BorderLayout.WEST);
     }
 
     // You can add more methods as needed
